2fe68aece8e4f9eb7f5b45d15d3aaa0ff23493c8
AminulIslamorange <aminulislamorange@gmail.com>
2024-12-07 18:26:26 +0600

    helment added

---
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -0,0 +1,16 @@
+import { Helmet } from "react-helmet-async";
+
+
+const Menu = () => {
+    return (
+        <div>
+            <Helmet>
+        <title>Bistro | menu</title>
+        
+      </Helmet>
+            
+        </div>
+    );
+};
+
+export default Menu;
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -4,9 +4,8 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     const navOption=<>
     <li><Link to='/'>Home</Link></li>
-    {/* <li><Link to='/menu'>Our Menu</Link></li>
-    <li><Link to='/order/salad'>Order</Link></li>
-    <li><Link to='login'>Login</Link></li> */}
+     <li><Link to='/menu'>Our Menu</Link></li> 
+     
     </>
     return (
         <div>
