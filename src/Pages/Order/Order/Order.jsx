2d1fc7946bed37004e0444d8d3e28cbb1ce0bccf
AminulIslamorange <aminulislamorange@gmail.com>
2024-12-08 17:20:23 +0600

    order page added

---
+++ src/Pages/Order/Order/Order.jsx
@@ -0,0 +1,11 @@
+import orderCoverImg from '../../../assets/shop/banner2.jpg';
+
+const Order = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Order;
