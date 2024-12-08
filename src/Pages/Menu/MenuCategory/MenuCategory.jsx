f54e41d63599afe14e7fa6b61b9746783e8e2dfb
AminulIslamorange <aminulislamorange@gmail.com>
2024-12-08 17:04:28 +0600

    menucategory page added

--- src/Pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/Pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,8 +1,25 @@
+import { Link } from "react-router-dom";
+import Cover from "../../Shared/Cover/Cover";
+import MenuItem from "../../Shared/Menuitem/Menuitem";
 
 
-const MenuCategory = () => {
+const MenuCategory = ({items,title,coverImg,details}) => {
     return (
-        <div>
+        <div >
+            { title && <Cover img={coverImg} title={title} details={details}></Cover>}
+            <div className="grid md:grid-cols-2 gap-6 mt-16">
+            {items.map(item=><MenuItem 
+            key={item._id} 
+            item={item}>
+
+            </MenuItem>)}
+            </div>
+            <div className="text-center">
+                <Link> 
+                <button className="btn btn-outline border-0 border-b-4 mb-4 ">View Full  Menu</button>
+                </Link>
+
+            </div>
             
         </div>
     );
