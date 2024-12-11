c43dfb380c2a31198ec8c10ea6af01f0af8cd1d9
AminulIslamorange <aminulislamorange@gmail.com>
2024-12-11 20:24:38 +0600

    context api added

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {RouterProvider} from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import { router } from './Routes/Routes.jsx';
 import { HelmetProvider } from 'react-helmet-async';
+import AuthProvider from './provider/AuthProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
- <div className='max-w-screen-xl mx-auto'> 
- <HelmetProvider>
-  <RouterProvider router={router} />
-  </HelmetProvider>
-  </div>
+    <div className='max-w-screen-xl mx-auto'>
+      <AuthProvider>
+        <HelmetProvider>
+          <RouterProvider router={router} />
+        </HelmetProvider>
+      </AuthProvider>
+    </div>
   </StrictMode>,
 )
