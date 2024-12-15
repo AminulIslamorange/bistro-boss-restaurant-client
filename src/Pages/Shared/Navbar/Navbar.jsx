ed151db2c09f02f9934230161259b8014096a4b0
AminulIslamorange <aminulislamorange@gmail.com>
2024-12-15 20:03:55 +0600

    useAxiosSecure hooks added

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,64 +1,69 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../provider/AuthProvider";
+import { FaShoppingCart } from "react-icons/fa";
 
 
 const Navbar = () => {
-  const {user,logOut}=useContext(AuthContext);
-  const handleLogOut=()=>{
+  const { user, logOut } = useContext(AuthContext);
+  const handleLogOut = () => {
     logOut()
-    .then(()=>{})
-    .catch(error=>console.error(error))
+      .then(() => { })
+      .catch(error => console.error(error))
 
   }
-    const navOption=<>
+  const navOption = <>
     <li><Link to='/'>Home</Link></li>
-     <li><Link to='/menu'>Our Menu</Link></li> 
-     <li><Link to='/order/salad'>Order</Link></li> 
-     
-     {user ? <li><button onClick={handleLogOut} className="btn btn-ghost">LogOut</button></li> 
-      :<li><Link to='/login'>Login</Link></li> }
-    </>
-    return (
-        <div>
-        <div className="navbar fixed z-30 bg-opacity-30 bg-black text-white mx-auto max-w-screen-xl">
-<div className="navbar-start">
-<div className="dropdown">
-  <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-5 w-5"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor">
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        d="M4 6h16M4 12h8m-8 6h16" />
-    </svg>
-  </div>
-  <ul
-    tabIndex={0}
-    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-    {navOption}
-    
-  </ul>
-</div>
-<a className="btn btn-ghost text-xl">NOURISH BISTRO<br/>Restaurant</a>
-</div>
-<div className="navbar-center hidden lg:flex">
-<ul className="menu menu-horizontal px-1">
-  {navOption}
-</ul>
-</div>
-<div className="navbar-end">
-<a className="btn">{user && user.displayName}</a>
-</div>
-</div>
-        
+    <li><Link to='/menu'>Our Menu</Link></li>
+    <li><Link to='/order/salad'>Order</Link></li>
+    <li><Link to='/'><button className="btn">
+    <FaShoppingCart  className="mr-2"/>
+      <div className="badge badge-secondary">+99</div>
+    </button></Link></li>
+
+    {user ? <li><button onClick={handleLogOut} className="btn btn-ghost">LogOut</button></li>
+      : <li><Link to='/login'>Login</Link></li>}
+  </>
+  return (
+    <div>
+      <div className="navbar fixed z-30 bg-opacity-30 bg-black text-white mx-auto max-w-screen-xl">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16" />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+              {navOption}
+
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl">NOURISH BISTRO<br />Restaurant</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            {navOption}
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">{user && user.displayName}</a>
+        </div>
+      </div>
+
     </div>
-    );
+  );
 };
 
 export default Navbar;
--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,24 +1,89 @@
+import Swal from "sweetalert2";
+import useAuth from "../../hooks/useAuth";
+import { useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 
-const FoodCard = ({item}) => {
-    const{price,image,recipe,name}=item;
-    return (
-        <div className="card bg-base-100 w-96 shadow-xl relative">
-            <figure>
-                <img
-                    src={image}
-                    alt="Shoes" />
-            </figure>
-            <p className="absolute right-3 top-2 text-white bg-black p-1">$:{price}</p>
-            <div className="card-body text-center">
-                <h2 className="card-title">{name}</h2>
-                <p>{recipe}</p>
-                <div className="card-actions justify-center">
-                    <button className="btn btn-outline border-0 border-b-4 my-4 text-[#BB8506] bg-slate-100">Add To Cart</button>
-                </div>
+
+const FoodCard = ({ item }) => {
+    const { price, image, recipe, name,_id } = item;
+    const { user } = useAuth();
+    const navigate = useNavigate();
+    const location=useLocation();
+
+    const handleAddToCart = (food) => {
+        if(user && user.email){
+            // send item to the database
+            const cartItem={
+                menuId:_id,
+                email:user.email,
+                name,
+                image,
+                price,
+
+            }
+            axios.post('http://localhost:5000/carts',cartItem)
+                .then(res=>{
+                    console.log(res.data);
+                    if(res.data.insertedId
+                    ){
+                        Swal.fire({
+                            position: "top-end",
+                            icon: "success",
+                            title: `Your ${name} added Successfully`,
+                            showConfirmButton: false,
+                            timer: 1500
+                          });
+
+                    }
+                })
+                
+            
+
+        }
+        else{
+            Swal.fire({
+                title: "You are not logged In",
+                text: "Please login to add to the cart",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, Login!"
+              }).then((result) => {
+                if (result.isConfirmed) {
+                    //   send the user to the login page
+                    navigate('/login', { state: { from: location } })
+                }
+              });
+
+
+
+        }
+       
+    
+
+}
+    
+return (
+    <div className="card bg-base-100 w-96 shadow-xl relative">
+        <figure>
+            <img
+                src={image}
+                alt="Shoes" />
+        </figure>
+        <p className="absolute right-3 top-2 text-white bg-black p-1">$:{price}</p>
+        <div className="card-body text-center">
+            <h2 className="card-title">{name}</h2>
+            <p>{recipe}</p>
+            <div className="card-actions justify-center">
+                <button
+                    onClick={() => handleAddToCart(item)}
+                    className="btn btn-outline border-0 border-b-4 my-4 text-[#BB8506] bg-slate-100">Add To Cart</button>
             </div>
         </div>
-    );
+    </div>
+);
 };
 
 export default FoodCard;
