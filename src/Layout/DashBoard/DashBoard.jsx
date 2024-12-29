c1e4fb6563a722c70170bcbb85217d0b8502df9b
AminulIslamorange <aminulislamorange@gmail.com>
2024-12-29 11:49:30 +0600

    admin panel setup

--- src/Layout/DashBoard/DashBoard.jsx
+++ src/Layout/DashBoard/DashBoard.jsx
@@ -1,17 +1,43 @@
-import { FaCalendarAlt, FaShoppingBag, FaShoppingCart } from "react-icons/fa";
+import { FaCalendarAlt, FaList, FaShoppingBag, FaShoppingCart, FaUsers, FaUtensils } from "react-icons/fa";
 import { IoHomeOutline, IoMenu } from "react-icons/io5";
 import { MdContactPhone, MdPayment, MdRateReview } from "react-icons/md";
 import { TbBrandBooking } from "react-icons/tb";
 import { NavLink, Outlet } from "react-router-dom";
 import useCart from "../../hooks/useCart";
+import { SiFacebookgaming } from "react-icons/si";
 
 const DashBoard = () => {
     const [cart]=useCart();
+    const isAdmin=true;
     return (
         <div className="flex">
             <div className="w-64 min-h-screen bg-[#D1A054]">
                 {/* dashboard sidebar */}
                 <ul className="menu">
+                {isAdmin?<>
+                    <li>
+                        
+                        <NavLink to='/dashboard/userHome'>  <IoHomeOutline /> Admin Home</NavLink>
+                    </li>
+                    <li>
+                        
+                        <NavLink to='/dashboard/addItems'> <FaUtensils></FaUtensils>  Add Items ({cart.length})</NavLink>
+                    </li>
+                   
+                    <li>
+                        
+                        <NavLink to='/dashboard/manageItem'>  <FaList></FaList> manage items</NavLink>
+                    </li>
+                    <li>
+                        
+                        <NavLink to='/dashboard/payment'> <SiFacebookgaming></SiFacebookgaming>  Manage bookings</NavLink>
+                    </li>
+                    <li>
+                        
+                        <NavLink to='/dashboard/users'> <FaUsers></FaUsers> all users</NavLink>
+                    </li>
+                    </>
+                :<>
                 <li>
                         
                         <NavLink to='/dashboard/userHome'>  <IoHomeOutline /> User Home</NavLink>
@@ -37,6 +63,8 @@ const DashBoard = () => {
                         
                         <NavLink to='/dashboard/booking'><TbBrandBooking />  my booking</NavLink>
                     </li>
+                </>
+                }
                     {/* shared navLink */}
                     <div className="divider"></div>
                     <li>
