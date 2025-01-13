65ada3c633f9b1eb18566e06a40999eb6b9bd59b
AminulIslamorange <aminulislamorange@gmail.com>
2025-01-13 20:07:56 +0600

    jwt added localstroage

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile,GoogleAuthProvider } from "firebase/auth";
 import app from "../firebase/firebase.config";
+import useAxiosPublic from "../hooks/useAxiosPublic";
 
 
 export const AuthContext=createContext(null);
@@ -8,6 +9,9 @@ const auth = getAuth(app);
 const AuthProvider = ({children}) => {
     const [user,setUser]=useState(null);
     const[loading,setLoading]=useState(true);
+    const axiosPublic=useAxiosPublic();
+
+
     const googleProvider=new GoogleAuthProvider();
 
     const googleSignIn=()=>{
@@ -41,10 +45,18 @@ const AuthProvider = ({children}) => {
             setUser(currentUser);
             if(currentUser){
                 // get token and store data
+                const userInfo={email:currentUser.email}
+                axiosPublic.post('/jwt',userInfo)
+                .then(res=>{
+                   if( res.data.token){
+                    localStorage.setItem('access-token',res.data.token)
+                   }
+                })
 
             }
             else{
                 // do something
+                localStorage.removeItem('access-token')
             }
             setLoading(false)
         });
