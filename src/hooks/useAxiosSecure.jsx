7cfbcb9bab1b96172e2b0e2fba1dc1e9ff08c0d7
AminulIslamorange <aminulislamorange@gmail.com>
2025-01-18 17:33:15 +0600

    jwt added in useaxiossecure

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -5,6 +5,29 @@ const axiosSecure=axios.create({
 
 })
 const useAxiosSecure = () => {
+    axiosSecure.interceptors.request.use(function(config){
+        const token=localStorage.getItem('access-token')
+        console.log(token);
+        config.headers.authorization=`Bearer ${token}`
+        return config
+    },function(error){
+        return Promise.reject(error)
+    })
+
+
+
+    // for interseptor 401 and 403
+    axiosSecure.interceptors.response.use(function (response) {
+        
+        return response;
+      }, function (error) {
+        const status=error.response.status;
+        console.log(status);
+        
+        return Promise.reject(error);
+      });
+
+
     return axiosSecure;
 
 
